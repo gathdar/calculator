9a1500a3c427f8372ae2b68cba23039d479377d0
Justin <theoperaking@yahoo.com>
2018-05-17 13:25:36 -0700

    got if statement to work by using querySelect

--- javascript_function.js
+++
@@ -1,8 +0,0 @@
-function calculate() {
-			var num1 = document.getElementById("num1").value;
-			var num2 = document.getElementById("num2").value;
-			var operation = document.getElementsByName("operation").value;
-			var total = num1 + num2;
-			var compute = document.getElementById("answer").innerHTML = "num1";
-			num1.value = ""; num2.value= "";
-			}
